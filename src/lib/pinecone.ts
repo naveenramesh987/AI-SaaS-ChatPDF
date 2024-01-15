7b8c512f37aa2034bb29e6ac123fdaa51d7ae7dc
Naveen <naveenramesh987@gmail.com>
2024-01-14 21:37:54 -0700

    pinecone

---
+++ src/lib/pinecone.ts
@@ -0,0 +1,23 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { downloadFromS3 } from "./s3-server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+let pinecone: Pinecone | null = null;
+
+export const getPineconeClient = () => {
+  return new Pinecone({
+    environment: process.env.PINECONE_ENVIRONMENT!,
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+};
+
+export async function loadS3IntoPinecone(fileKey: string) {
+  console.log("downloading s3 into file system");
+  const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("could not download from s3");
+  }
+  const loader = new PDFLoader(file_name);
+  const pages = await loader.load();
+  return pages;
+}
